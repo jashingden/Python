e72b2b8ff95e06ec86735bd7f10f0d6f57969a6e
eddyteng <eddyteng@mitake.com.tw>
2021-01-05 14:35:51 +0800

    新增相似K線

--- PandasTest.py
+++ PandasTest.py
@@ -16,10 +16,85 @@ Created on Wed Feb 22 17:57:59 2017
 
 import pandas as pd
 import numpy as np
+import csv
 
-df = pd.read_csv('D:\MITAKE\VSProject\MitakePortfolio\GPHONE_MTK.csv',
-        sep='\t', names=['PID', 'Org', 'UserID', 'Stocks', 'SN', 'HID', 'UpdateDT'])
+#df = pd.read_csv('D:\MITAKE\VSProject\MitakePortfolio\GPHONE_MTK.csv',
+#        sep='\t', names=['PID', 'Org', 'UserID', 'Stocks', 'SN', 'HID', 'UpdateDT'])
+#
+#df.head()
+#
+#df['Stocks'].iloc[3].split('@')[2]
 
-df.head()
+def get_k(stk):
+    o = stk[:,3]
+    o = o.astype(np.float)
+    h = stk[:,4]
+    h = h.astype(np.float)
+    l = stk[:,5]
+    l = l.astype(np.float)
+    c = stk[:,6]
+    c = c.astype(np.float)
+    return [o, h, l, c]
 
-df['Stocks'].iloc[3].split('@')[2]
+def get_corrc(k1, k2):
+    o = round(np.corrcoef(k1[0], k2[0])[0][1], 3)
+    h = round(np.corrcoef(k1[1], k2[1])[0][1], 3)
+    l = round(np.corrcoef(k1[2], k2[2])[0][1], 3)
+    c = round(np.corrcoef(k1[3], k2[3])[0][1], 3)
+    corrc = (o + h + l + c) / 4
+    return corrc
+
+def saveCSV(path, mylist):
+    file = open(path, 'w', newline='')
+    writer = csv.writer(file)
+    writer.writerows(mylist)
+
+in_file = 'D:\MITAKE\VSProject\MitakeSmartV2\Project\deploy\DailyK_20210104.csv'
+out_file = 'D:\MITAKE\VSProject\MitakeSmartV2\Project\deploy\SimilarK_20210104.csv'
+
+all_stock = pd.read_csv(in_file, header=1, dtype='str')
+all_stock = np.array(all_stock)
+
+all_id = all_stock[:,0]
+base_id = '6488'
+stk_info = []
+stk_id = ''
+base_idx = 0
+k_len = 1000
+comp_len = 100
+info = []
+
+for i in range(len(all_id)):
+    if stk_id != all_id[i]:
+        if len(info) > 0:
+            stk_info.append(info)
+        stk_id = all_id[i]
+        info = [stk_id, i, i, 0.0]
+        if stk_id == base_id:
+            base_idx = len(stk_info)
+    else:
+        info[2] = i
+
+if len(info) > 0:
+    stk_info.append(info)
+
+base_info = stk_info[base_idx]
+end = base_info[2] + 1
+start = end - comp_len
+stk_base = all_stock[start:end]
+k_base = get_k(stk_base)
+
+for info in stk_info:
+    end = info[2] + 1
+    start = end - comp_len
+    if start >= info[1]:
+        stk = all_stock[start:end]
+        k = get_k(stk)
+        info[3] = get_corrc(k_base, k)
+
+stk_sort = np.array(stk_info)
+sort = stk_sort[:,3]
+sort = sort.astype(np.float)
+stk_sort = stk_sort[np.argsort(sort)][::-1]
+
+saveCSV(out_file, stk_sort)
